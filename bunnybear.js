16022fd6114703fb1d258f74bed586730f447ea7
Mohammed Hussain <mhussain@Mohammeds-MacBook-Pro.local>
2018-08-24 11:26:29 -0400

    .

--- bunnybear.js
+++ bunnybear.js
@@ -1,1 +1,11 @@
-// lets try again.
+const bunnyslack = require('./bunnyslack');
+const wake = require('./wake/wake');
+
+(() => {
+    const body = new bunnyslack({
+        token: 'xoxb-414980923588-416254894241-bgd20aDgjbDYyhCfsJ9F4Bry',
+        name: 'bunnybear'
+    });
+
+    const up = new wake(body);
+})();
---
+++ wake/wake.js
@@ -0,0 +1,62 @@
+const _ = require('underscore');
+
+// wake the vessel
+
+class wake {
+    constructor(vessel) {
+        this.body = vessel;
+        this.pass = 0;
+        this.fail = 0;
+        this.warning = 0;
+        this._wake_getchannels();
+
+        // this needs time
+        //this._report();
+    };
+
+    _report() {
+        console.log('yes ' + this.pass);
+        console.log('no ' + this.fail);
+        console.log('maybe ' + this.warning);
+    }
+
+    _wake_getchannels() {
+        this.body.getChannels().then((res) => {
+            const channels = res.channels;
+            const gchannel = _.find(channels, (c) => { return c.name === 'general' });
+            const rchannel = _.find(channels, (c) => { return c.name === 'random' });
+
+            if (gchannel) {
+                console.log('i can reach generally');
+                this.pass++;
+            } else {
+                console.log('i cannot reach generally');
+                this.fail++;
+            }
+
+            if (rchannel) {
+                console.log('i can reach randomly');
+                this.pass++;
+            } else {
+                console.log('i cannot reach randomly');
+                this.fail++;
+            }
+        });
+    };
+
+    yes() {
+        return this.pass;
+    };
+
+    no() {
+        return this.fail;
+    };
+
+    maybe() {
+        return this.warning;
+    };
+}
+
+
+
+module.exports = wake;
