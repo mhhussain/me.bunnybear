12b95a218f2ab8aeaf8a0a02463fb884a090168f
b <b>
2018-09-01 00:30:43 -0400

    .

--- bunnybear.js
+++ bunnybear.js
@@ -5,6 +5,20 @@ const tokens = require('./secrets');
 var cmdmode = false;
 var cmdbag = {};
 
+const handlecommand = (cmd) => {
+    if (cmdmode) {
+        const evcmd = cmd.substr(5)
+            .replace(/‘/g, String.fromCharCode(39))
+            .replace(/’/g, String.fromCharCode(39));
+        console.log(cmd);
+        console.log(cmdmode);
+        console.log(evcmd);
+        eval(evcmd);
+    } else {
+        return;
+    }
+}
+
 const handlemessage = (message) => {
     if (message.user != 'UC72G0ATD') {
         return;
@@ -17,17 +31,15 @@ const handlemessage = (message) => {
     const cmd = message.text;
 
     if (cmd.substr(0,4) === '::cm') {
-        if (cmdmode) {
-            const evcmd = cmd.substr(5)
-                .replace(/‘/g, String.fromCharCode(39))
-                .replace(/’/g, String.fromCharCode(39));
-            console.log(cmd);
-            console.log(cmdmode);
-            console.log(evcmd);
-            eval(evcmd);
-        } else {
-            return;
-        }
+        return handlecommand(cmd);
+    } else if (cmd === 'hi') {
+        body.postMessageToUser('moohh91', 'hi').then(() => {
+            body.postMessageToUser('moohh91', 'go away');
+        });
+    } else if (cmd === 'you\'re useless') {
+        body.postMessageToUser('moohh91', 'im a fucking computer').then(() => {
+            body.postMessageToUser('moohh91', 'on a fucking computer');
+        });
     }
 
     if (cmd === 'the piano should play moonlight sonata') {
@@ -51,5 +63,5 @@ const body = new bunnyslack({
 body.on('message', handlemessage);
 
 (() => {
-    const up = new wake(body);
+    //const up = new wake(body);
 })();
--- secrets.js
+++ secrets.js
@@ -1,6 +1,8 @@
 const tokens = {
-    bot: '',
-    ws: ''
+    bot: 'xoxb-414980923588-416254894241-3g5lXU3ArU6ZOpSRL1qGQ6f2',
+    ws: 'xoxp-414980923588-415084010931-425961391858-98ae05141e8d322bcf68d4c71b72cdc9',
+    xbt_bot: '',
+    xbt_ws: ''
 };
 
 module.exports = tokens;
