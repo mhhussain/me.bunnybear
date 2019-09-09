1311c3c3e1e82df33c5a99691b246f28508853ae
moohh <mo051191@yahoo.com>
2019-09-08 23:04:08 -0400

    tabula rasa

---
+++ ref/backmind/secrets.js
@@ -0,0 +1,8 @@
+const tokens = {
+    bot: 'xoxb-414980923588-416254894241-capklb8XDTQJvq4oGttWb6S9',
+    ws: 'xoxp-414980923588-415084010931-434708329527-805c429b092a70f615c4ab98555f9d98',
+    xbt_bot: '',
+    xbt_ws: ''
+};
+
+module.exports = tokens;
